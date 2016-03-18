56d0421758d08773439cda10716a1f3b88f84f11
Gareth Ellis <garethellis@gmail.com>
2016-03-18 14:02:46 +0000

    Fix bug with array values matcher description output + add constraints to make sure only array or Traversable are passed to matcher

--- src/Matcher/ArrayValuesMatcher.php
+++ src/Matcher/ArrayValuesMatcher.php
@@ -18,24 +18,35 @@ class ArrayValuesMatcher extends BaseMatcher
      */
     public function __construct($array)
     {
+        $this->assertArrayLike($array);
         $this->expected = $array;
     }
 
     public function matches($actual)
     {
+        $this->assertArraylike($actual);
         return $this->getValues($actual) === $this->getValues($this->expected);
     }
 
     public function describeTo(Description $description)
     {
-        $description->appendText($this->expected);
+        $description->appendValue($this->expected);
     }
 
     protected function getValues($arrayLike)
     {
-        if (!is_array($arrayLike)) {
+        if ($arrayLike instanceof \Traversable) {
             $arrayLike = iterator_to_array($arrayLike);
         }
         return array_values($arrayLike);
+    }
+
+    protected function assertArrayLike($array)
+    {
+        if (!is_array($array) && !$array instanceof \Traversable) {
+            throw new \InvalidArgumentException(
+                "Argument passed to ArrayValuesMatcher must be an array or an instance of Traversable"
+            );
+        }
     }
 }
--- test/Matcher/ArrayValuesMatcherTest.php
+++ test/Matcher/ArrayValuesMatcherTest.php
@@ -11,10 +11,18 @@ class ArrayValuesMatcherTest extends \PHPUnit_Framework_TestCase
         assertThat($actual, hasEqualValuesTo($expected));
     }
 
-    public function testArrayValuesMatcherWithAnArrayLikeObject()
+    public function testArrayValuesMatcherWithATraversableInstance()
     {
         $expected = ["foo", "bar", "baz"];
         $actual = new \ArrayObject([1 => "foo", 3 => "bar", 4 => "baz"]);
+        assertThat($actual, hasEqualValuesTo($expected));
+    }
+
+    public function testArrayValuesMatcherOnlyAcceptsArraysAndTraversable()
+    {
+        $expected = "foo";
+        $actual = new \ArrayObject([1 => "foo", 3 => "bar", 4 => "baz"]);
+        $this->expectException(\InvalidArgumentException::class);
         assertThat($actual, hasEqualValuesTo($expected));
     }
 }
